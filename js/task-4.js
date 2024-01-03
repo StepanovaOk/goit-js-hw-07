f6c6913b173b774d0163c7fb55c83ea58ac3b823
StepanovaOk <143267396+StepanovaOk@users.noreply.github.com>
2024-01-03 23:47:03 +0200

    4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,39 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const email = loginForm.elements.email.value.trim();
+  const password = loginForm.elements.password.value.trim();
+
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+  } else {
+    const userObject = {
+      email: email,
+      password: password,
+    };
+    console.log(userObject);
+  }
+
+  loginForm.reset();
+});
+
+// const registerForm = document.querySelector('.login-form');
+// const userData = { email: '', password: '' };
+
+// const onSubmit = event => {
+//   event.preventDefault();
+//   const email = event.target.elements.email.value.trim();
+//   const password = event.target.elements.password.value.trim();
+//   if (email && password) {
+//     userData.email = email;
+//     userData.password = password;
+//     console.log(userData);
+//     event.target.reset();
+//   } else {
+//     alert('All form fields must be filled in');
+//   }
+// };
+
+// registerForm.addEventListener('submit', onSubmit);
