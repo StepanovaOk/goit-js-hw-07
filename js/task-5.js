3e8954138a0cda13f4028f89b17b82df62c1b22d
StepanovaOk <143267396+StepanovaOk@users.noreply.github.com>
2024-01-04 19:04:07 +0200

    5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
+const btnChange = document.querySelector('.change-color');
+const backgroundSpan = document.querySelector('.color');
+const body = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btnChange.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  backgroundSpan.textContent = newColor;
+});
