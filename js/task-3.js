e3cad5eb7a4ebd41a099d59ec5421ff29eb2e10e
StepanovaOk <143267396+StepanovaOk@users.noreply.github.com>
2024-01-07 16:55:44 +0200

    fixed

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,7 @@
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
-const textGreeting = document.querySelector('h1');
 
 nameInput.addEventListener('input', event => {
   const inputValue = nameInput.value.trim();
-  nameOutput.textContent = inputValue;
+  nameOutput.textContent = inputValue === '' ? 'Anonymous' : inputValue;
 });
